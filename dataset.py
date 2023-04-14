f746ada97dba9051bba710eafd944eba19cd5163
Ryan Lee <ryanl1288@hotmail.com>
2023-04-13 20:53:26 -0400

    Functional dataset

---
+++ dataset.py
@@ -0,0 +1,104 @@
+import os
+import time
+import random
+import torch
+import numpy as np
+import SharedArray as SA
+from torch.utils.data import Dataset
+
+from utils import sa_create, collate_fn, data_prepare
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def worker_init_fn(worker_id):
+    random.seed(manual_seed + worker_id)
+
+class TOScanNet(Dataset):
+    """
+    Args:
+        split (str): 'train' or 'test' split.
+        data_root (str): path to the root directory containing data in npz format.
+        split_root (str): path to the directory containing train and test splits.
+        voxel_size (float): size of the voxel in meters.
+        voxel_max (float): maximum number of voxels allowed.
+        transform (callable, optional): A function/transform that takes in the (x,y,z) coordinates and returns 
+            a transformed version. Default: None.
+        shuffle_index (int, optional): Index for shuffling the points. Default: False.
+        loop (int, optional): Number of times to loop over the data. Default: 1.
+        repeat_align (bool, optional): Repeat alignment? Default: False.
+
+    Attributes:
+        data_list (list): List containing names of all files in the split.
+        data_idx (numpy array): Array containing the indices of the data.
+    """
+    def __init__(self, split: str='train', data_root: str='trainval', split_root: str="list", \
+                 voxel_size: float=0.004, voxel_max: float=80000, transform=None, \
+                 shuffle_index: int=False, loop: int=1, repeat_align: bool=False):
+        super().__init__()
+        self.split, self.voxel_size, self.transform, self.voxel_max, self.shuffle_index, \
+        self.loop, self.repeat_align, self.data_root = split, voxel_size, transform, voxel_max, shuffle_index, \
+            loop, repeat_align, data_root
+
+        # get the data_list - all scenes listed inside the training set
+        data_list = os.path.join(split_root, f"{split}.txt")
+        with open(data_list) as d_list:
+            data_list = d_list.read().splitlines()
+        
+        self.data_list = data_list
+        self.data_idx = np.arange(len(self.data_list))
+
+        print(f"Totally {len(self.data_idx)} samples in {split} set.")
+
+    def __getitem__(self, idx):
+        data_idx = self.data_idx[idx % len(self.data_idx)]
+        data_path = os.path.join(self.data_root, self.data_list[data_idx] + '.npz')
+        f = np.load(data_path)
+        coord, feat, label = f['xyz'], f['color'], np.expand_dims(f['semantic_label'], axis=-1)
+        label = remapper[label.astype(int)]
+        coord, feat, label = data_prepare(coord, feat, label, self.split, self.voxel_size, \
+                self.voxel_max, self.transform, self.shuffle_index, self.repeat_align)
+        
+        return coord, feat, label
+
+    def __len__(self):
+        return len(self.data_idx) * self.loop
+
+
+if __name__ == '__main__':
+    DATA_ROOT = 'data/TO-scannet/train'
+    SPLIT_ROOT = 'data/meta_data/TO-scannet'
+
+    point_data = TOScanNet(split='train', data_root=DATA_ROOT, split_root=SPLIT_ROOT)
+    print('point data size:', len(point_data))
+
+    remapper = np.ones(300) * (255)
+    for i, x in enumerate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 16, 24, 28, 33, 34, 36, 39, 41, \
+                        42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, \
+                        60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, \
+                        78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 92, 93]):
+        remapper[x] = i
+
+    manual_seed = 123
+    set_seed(manual_seed)
+
+    train_loader = torch.utils.data.DataLoader(point_data, batch_size=16, shuffle=False, \
+                                               num_workers=0, pin_memory=True, collate_fn=collate_fn)
+    for idx in range(1):
+        end = time.time()
+        voxel_num = []
+        for i, (coord, feat, label, offset) in enumerate(train_loader):
+            print(f'time: {i+1}/{len(train_loader)}--{time.time() - end}')
+            print('tag', coord.shape, feat.shape, label.shape, offset.shape, torch.unique(label))
+            if torch.isnan(coord).any() or torch.isnan(feat).any() or torch.isnan(label).any():
+                print("!")
+                exit(0)
+            if torch.isinf(coord).any() or torch.isinf(feat).any() or torch.isinf(label).any():
+                print("!")
+                exit(0)
+            voxel_num.append(label.shape[0])
+            end = time.time()
+    print(np.sort(np.array(voxel_num)))
---
+++ utils.py
@@ -0,0 +1,109 @@
+import torch
+import random
+import SharedArray as SA
+import numpy as np
+
+from collections import Sequence
+
+def sa_create(name, var):
+    x = SA.create(name, var.shape, dtype=var.dtype)
+    x[...] = var[...]
+    x.flags.writeable = False
+    return x
+
+def collate_fn(batch):
+    coord, feat, label = list(zip(*batch))
+    offset, count = [], 0
+    for item in coord:
+        count += item.shape[0]
+        offset.append(count)
+    return torch.cat(coord), torch.cat(feat), torch.cat(label), torch.IntTensor(offset)
+
+def data_prepare(coord, feat, label, split='train', voxel_size=0.004, voxel_max=None, transform=None, shuffle_index=False, repeat_align=False):
+    if transform:
+        coord, feat, label = transform(coord, feat, label)
+    if voxel_size:
+        coord_min = np.min(coord, 0)
+        coord -= coord_min
+        uniq_idx = voxelize(coord, voxel_size)
+        coord, feat, label = coord[uniq_idx], feat[uniq_idx], label[uniq_idx]
+    if voxel_max and label.shape[0] > voxel_max:
+        init_idx = np.random.randint(label.shape[0]) if 'train' in split else label.shape[0] // 2
+        crop_idx = np.argsort(np.sum(np.square(coord - coord[init_idx]), 1))[:voxel_max]
+        coord, feat, label = coord[crop_idx], feat[crop_idx], label[crop_idx]
+    elif repeat_align and label.shape[0] <= voxel_max:  # if M <= num_pts, directly repeat:
+        coord, feat, label = data_dup(coord, feat, label, voxel_max)  # [num_point,cin]
+    if shuffle_index:
+        shuf_idx = np.arange(coord.shape[0])
+        np.random.shuffle(shuf_idx)
+        coord, feat, label = coord[shuf_idx], feat[shuf_idx], label[shuf_idx]
+
+    coord_min = np.min(coord, 0)
+    coord -= coord_min
+    coord = torch.FloatTensor(coord)
+    feat = torch.FloatTensor(feat) / 255.
+    label = torch.LongTensor(label)
+    return coord, feat, label
+
+def voxelize(coord, voxel_size=0.05, hash_type='fnv', mode=0):
+    discrete_coord = np.floor(coord / np.array(voxel_size))
+    if hash_type == 'ravel':
+        key = ravel_hash_vec(discrete_coord)
+    else:
+        key = fnv_hash_vec(discrete_coord)
+
+    idx_sort = np.argsort(key)
+    key_sort = key[idx_sort]
+    _, count = np.unique(key_sort, return_counts=True)
+    if mode == 0:  # train mode
+        idx_select = np.cumsum(np.insert(count, 0, 0)[0:-1]) + np.random.randint(0, count.max(), count.size) % count
+        idx_unique = idx_sort[idx_select]
+        return idx_unique
+    else:  # val mode
+        return idx_sort, count
+
+def fnv_hash_vec(arr):
+    """
+    FNV64-1A
+    """
+    assert arr.ndim == 2
+    # Floor first for negative coordinates
+    arr = arr.copy()
+    arr = arr.astype(np.uint64, copy=False)
+    hashed_arr = np.uint64(14695981039346656037) * np.ones(arr.shape[0], dtype=np.uint64)
+    for j in range(arr.shape[1]):
+        hashed_arr *= np.uint64(1099511628211)
+        hashed_arr = np.bitwise_xor(hashed_arr, arr[:, j])
+    return hashed_arr
+
+
+def ravel_hash_vec(arr):
+    """
+    Ravel the coordinates after subtracting the min coordinates.
+    """
+    assert arr.ndim == 2
+    arr = arr.copy()
+    arr -= arr.min(0)
+    arr = arr.astype(np.uint64, copy=False)
+    arr_max = arr.max(0).astype(np.uint64) + 1
+
+    keys = np.zeros(arr.shape[0], dtype=np.uint64)
+    # Fortran style indexing
+    for j in range(arr.shape[1] - 1):
+        keys += arr[:, j]
+        keys *= arr_max[j + 1]
+    keys += arr[:, -1]
+    return keys
+
+def data_dup(xyz, color, label, num_out):
+    num_in = label.shape[0]
+    dup = np.random.choice(num_in, num_out - num_in)
+    xyz_dup = xyz[dup, ...]
+    xyz_dup = np.concatenate([xyz, xyz_dup], 0)
+    color_dup = color[dup, ...]
+    color_dup = np.concatenate([color, color_dup], 0)
+    label = np.expand_dims(label, axis=-1)
+    label_dup = label[dup]
+    label_dup = np.concatenate([label, label_dup], 0)
+    label_dup = label_dup.squeeze(-1)
+    return xyz_dup, color_dup, label_dup
